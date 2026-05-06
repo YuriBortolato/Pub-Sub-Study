77547e2fba0cc86b523c44989d63b1aae269af9f
YuriBortolato <yurigbort@gmail.com>
2026-05-06 19:48:06 -0300

    feat: add initial RabbitMQ publisher and consumer console apps

---
+++ umfg.consumer.console/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace umfg.consumer.console;
+
+internal sealed class Program
+{
+    private const string C_CONNECTION = "amqps://srwrdfzu:i59_Ow2ni7Qayn4UecU30KFSIU-v2-NX@jackal.rmq.cloudamqp.com/srwrdfzu";
+    private const string C_QUEUE = "umfg-programacao-iv-teste-2026";
+
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                Uri = new Uri(C_CONNECTION),
+            };
+            var connection = await connectionFactory.CreateConnectionAsync();
+            var channel = await connection.CreateChannelAsync();
+
+            Console.WriteLine("Inicio envio de mensagens!");
+
+            await channel.QueueDeclareAsync(queue: C_QUEUE,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+
+            consumer.ReceivedAsync += ConsumerReceivedAsync; //delegado
+
+            await channel.BasicConsumeAsync(queue: C_QUEUE, autoAck: true, consumer: consumer);
+
+            Console.WriteLine("Pressione [enter] para sair.");
+            Console.ReadLine();
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    private static async Task ConsumerReceivedAsync(object sender, BasicDeliverEventArgs e)
+    {
+        await Task.Run(() =>
+        {
+            Console.WriteLine($"[Nova mensagem | {DateTime.Now:yyyy-MM-dd HH:mm:ss}] {Encoding.UTF8.GetString(e.Body.ToArray())}");
+        });
+    }
+}
---
+++ umfg.publisher.console/Program.cs
@@ -0,0 +1,60 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace umfg.publisher.console
+{
+    internal class Program
+    {
+        private const string C_CONNECTION = "amqps://srwrdfzu:i59_Ow2ni7Qayn4UecU30KFSIU-v2-NX@jackal.rmq.cloudamqp.com/srwrdfzu";
+        private const string C_QUEUE = "umfg-programacao-iv-teste-2026";
+
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var connectionFactory = new ConnectionFactory()
+                {
+                    Uri = new Uri(C_CONNECTION),
+                };
+                var connection = await connectionFactory.CreateConnectionAsync();
+                var channel = await connection.CreateChannelAsync();
+
+                Console.WriteLine("Inicio envio de mensagens!");
+
+                await channel.QueueDeclareAsync(queue: C_QUEUE,
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                foreach (var numero in Enumerable.Range(1, 10))
+                {
+                    /*
+                     * (string exchange, string routingKey,
+            bool mandatory, TProperties basicProperties, ReadOnlyMemory<byte> body,
+            CancellationToken cancellationToken = default)
+                     */
+
+                    var properties = new BasicProperties
+                    {
+                        Persistent = true
+                    };
+
+                    await Task.Delay(TimeSpan.FromSeconds(1));
+                    await channel.BasicPublishAsync("",
+                        C_QUEUE,
+                        false,
+                        properties,
+                        Encoding.UTF8.GetBytes($"{numero}º Hello Word!"),
+                        CancellationToken.None);
+                }
+
+                Console.WriteLine("Fim envio de mensagens!");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
